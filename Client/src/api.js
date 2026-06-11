ae7b542b76482784b08f62c7a22f56a2b7bbb91c
Keerthivarman <your-email@gmail.com>
2026-06-11 11:31:19 +0530

    Update API URLs

--- Client/src/api.js
+++ Client/src/api.js
@@ -1,4 +1,10 @@
+// export const API = {
+//   ADMIN: "http://localhost:7001",
+//   CANDIDATE: "http://localhost:7002"
+// };
+
+
 export const API = {
-  ADMIN: "http://localhost:7001",
-  CANDIDATE: "http://localhost:7002"
+  ADMIN: "https://curioussoftware-2.onrender.com/",
+  CANDIDATE: "https://curioussoftware-1.onrender.com/"
 };
